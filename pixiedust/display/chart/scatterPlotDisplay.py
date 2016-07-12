365f5c0480b0c8c7770cd1d1660928c06936a525
Margriet Groenendijk <margriet@margriets-air.bristol.uk.ibm.com>
2016-07-12 15:29:33 +0100

    scatter plot

--- pixiedust/display/chart/scatterPlotDisplay.py
+++ pixiedust/display/chart/scatterPlotDisplay.py
@@ -15,6 +15,36 @@
 # -------------------------------------------------------------------------------
 
 from .display import ChartDisplay
-    
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+import mpld3
+     
 class ScatterPlotDisplay(ChartDisplay):
-    pass
+    def doRender(self, handlerId):
+        displayColName = self.getNumericalColsInfo()
+        if len(displayColName) < 2:
+            self._addHTML("Unable to find two numerical columns in the dataframe")
+            return
+        
+        mpld3.enable_notebook()        
+        x = self.entity.select(displayColName[0]).toPandas()[displayColName[0]].dropna().tolist()
+        y = self.entity.select(displayColName[1]).toPandas()[displayColName[1]].dropna().tolist()
+
+        plt.rcParams['font.size']=11
+        plt.rcParams['figure.figsize']=[6.0, 5.0]
+        fig, ax=plt.subplots(subplot_kw=dict(axisbg='#EEEEEE'))          
+        ax.grid(color='white', linestyle='solid')
+        scatter = ax.scatter(x,y,c=y,marker='o',alpha=0.7,s=124,cmap=plt.cm.ocean)
+        ax.set_title("D3 Scatter Plot", size=18);
+        ax.set_xlabel(displayColName[0], size=14)
+        ax.set_ylabel(displayColName[1], size=14)
+            
+    def getNumericalColsInfo(self):
+        schema = self.entity.schema
+        fields = []
+        for field in schema.fields:
+            type = field.dataType.__class__.__name__
+            if ( type =="LongType" or type == "IntegerType" ):
+                fields = np.append(fields,field.name)
+        return fields
