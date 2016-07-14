cdd5f72686ef646211af4d1754d0a8432e941ef4
David Taieb <david_taieb@us.ibm.com>
2016-07-14 10:58:12 -0400

    added download to list of packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pixiedust',
-      version='0.14',
+      version='0.16',
       description='Misc helpers for Spark Python Notebook',
       url='https://github.com/ibm-cds-labs/pixiedust',
       install_requires=['maven-artifact','mpld3'],
@@ -9,6 +9,6 @@ setup(name='pixiedust',
       author_email='david_taieb@us.ibm.com',
       license='Apache 2.0',
       packages=['pixiedust','pixiedust.packageManager','pixiedust.display',
-            'pixiedust.display.table','pixiedust.display.graph','pixiedust.display.chart','pixiedust.display.tests',
+            'pixiedust.display.table','pixiedust.display.graph','pixiedust.display.chart','pixiedust.display.tests','pixiedust.display.download',
             'pixiedust.services'],
       zip_safe=False)
