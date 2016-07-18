f941c6d7531ea33500acb9aed5e4b70eadb83361
David Taieb <david_taieb@us.ibm.com>
2016-07-18 17:56:23 -0400

    fixed setup.py to include static templates

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pixiedust',
-      version='0.16',
+      version='0.19',
       description='Misc helpers for Spark Python Notebook',
       url='https://github.com/ibm-cds-labs/pixiedust',
       install_requires=['maven-artifact','mpld3'],
@@ -9,7 +9,9 @@ setup(name='pixiedust',
       author_email='david_taieb@us.ibm.com',
       license='Apache 2.0',
       packages=['pixiedust','pixiedust.packageManager','pixiedust.display',
-            'pixiedust.display.table','pixiedust.display.graph','pixiedust.display.chart','pixiedust.display.tests','pixiedust.display.download',
+            'pixiedust.display.table','pixiedust.display.graph','pixiedust.display.chart','pixiedust.display.chart.plugins',
+            'pixiedust.display.tests','pixiedust.display.download',
             'pixiedust.services',
             'pixiedust.utils'],
+      include_package_data=True,
       zip_safe=False)
