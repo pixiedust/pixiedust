0f09600c51f6269a7565ff59c8519c9492cf7ae1
Mike Broberg <mikebroberg@gmail.com>
2016-07-21 16:35:41 -0400

    Pie chart with sampling and aggregate functions

--- pixiedust/display/chart/pieChartDisplay.py
+++ pixiedust/display/chart/pieChartDisplay.py
@@ -14,57 +14,71 @@
 # limitations under the License.
 # -------------------------------------------------------------------------------
 
-from .display import ChartDisplay
-from pylab import *
-import mpld3
+from .mpld3ChartDisplay import Mpld3ChartDisplay
 from pyspark.sql import functions as F
     
-class PieChartDisplay(ChartDisplay):
-    def doRender(self, handlerId):
-        displayColName = self.getSmartNumericalColInfo()
+class PieChartDisplay(Mpld3ChartDisplay):
+    def doRenderMpld3(self, handlerId, figure, axes):
+        agg = self.options.get("aggregation", "count")
+        displayColName = self.getPieColInfo(agg != "count")
         if not displayColName:
-            self._addHTML("Unable to find a numerical column in the dataframe")
+            self._addHTML("Unable to find a suitable column in the dataframe")
             return
 
-        mpld3.enable_notebook() 
-        # make a square figure and axes
-        figure(1, figsize=(9,9))
-        ax = axes([0.1, 0.1, 0.8, 0.8])
-
-        # The slices will be ordered and plotted counter-clockwise.
-        #labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-        #fracs = [15, 30, 45, 10]
-        #explode=(0, 0, 0.1, 0)
-
-        pand = self.entity.groupBy(displayColName).agg(F.count(displayColName).alias("count")).toPandas()
-        x = pand["count"].dropna().tolist()
+        pand = self.handleUIOptions(displayColName)
+        x = pand["agg"].dropna().tolist()
         labels=pand[displayColName].tolist()
 
-        pie(x, labels=labels, explode=None, autopct='%1.1f%%', startangle=90)
-                        # The default startangle is 0, which would start
-                        # the Frogs slice on the x-axis.  With startangle=90,
-                        # everything is rotated counter-clockwise by 90 degrees,
-                        # so the plotting starts on the positive y-axis.
+        # The default startangle is 0. With startangle=90, everything is rotated counter-clockwise by 90 degrees
+        axes.pie(x, labels=labels, explode=None, autopct='%1.1f%%', startangle=90)
 
-        title(displayColName.lower() + ' totals', bbox={'facecolor':'0.8', 'pad':5})
 
-        show()
+        # numerical used as a boolean flag for truth table
+    def getPieColInfo(self, numerical):
+        # If user selects a column in dialog box, give it to them
+        keyFields = self.options.get("keyFields")
+        if keyFields is not None:
+            return keyFields
 
-    #TODO: adjust for 'uuid' corner case and sample to test for repeated values
-    def getSmartNumericalColInfo(self):
         schema = self.entity.schema
         default=None
         for field in schema.fields:
-            #ignore unique ids
-            if field.name.lower() != 'id':
-            #type = field.dataType.__class__.__name__
-            #if ( type =="LongType" or type == "IntegerType" ):
-
-            #Find a good column to display in pie ChartDisplay
+            # Ignore unique ids
+            if field.name.lower() != 'id' and ( not numerical or isNum(field.dataType.__class__.__name__) ):
+            # Find a good column to display in pie ChartDisplay
                 default = default or field.name
                 count = self.entity.count()
                 sample = self.entity.sample(False, (float(200) / count)) if count > 200 else self.entity
-                orderedSample = sample.groupBy(field.name).agg(F.count(field.name).alias("count")).orderBy(F.desc("count")).select("count")
-                if orderedSample.take(1)[0]["count"] > 10:
+                orderedSample = sample.groupBy(field.name).agg(F.count(field.name).alias("agg")).orderBy(F.desc("agg")).select("agg")
+                if orderedSample.take(1)[0]["agg"] > 10:
                     return field.name
+        # Otherwise, return first non-id column
         return default
+
+
+        # Handle user-defined aggregate functions
+    def handleUIOptions(self, displayColName):
+        agg = self.options.get("aggregation")
+        valFields = self.options.get("valueFields")
+
+        if agg == 'COUNT':
+            return self.entity.groupBy(displayColName).agg(F.count(displayColName).alias("agg")).toPandas()
+        elif agg == 'SUM':
+            return self.entity.groupBy(displayColName).agg(F.sum(valFields).alias("agg")).toPandas()
+        elif agg == 'AVG':
+            return self.entity.groupBy(displayColName).agg(F.avg(valFields).alias("agg")).toPandas()
+        elif agg == 'MIN':
+            return self.entity.groupBy(displayColName).agg(F.min(valFields).alias("agg")).toPandas()
+        elif agg == 'MAX':
+            return self.entity.groupBy(displayColName).agg(F.max(valFields).alias("agg")).toPandas()
+        elif agg == 'MEAN':
+            return self.entity.groupBy(displayColName).agg(F.mean(valFields).alias("agg")).toPandas()
+        else:
+            return self.entity.groupBy(displayColName).agg(F.count(displayColName).alias("agg")).toPandas()
+
+
+    def isNum(type):
+        if ( type =="LongType" or type == "IntegerType" ):
+            return true
+        else:
+            return false
