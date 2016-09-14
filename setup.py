74bc44f417c8140dbbf9e12f253a95302437a394
David Taieb <david_taieb@us.ibm.com>
2016-09-14 13:06:52 -0400

    version 0.32

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pixiedust',
-      version='0.31',
+      version='0.32',
       description='Misc helpers for Spark Python Notebook',
       url='https://github.com/ibm-cds-labs/pixiedust',
       install_requires=['maven-artifact','mpld3'],
