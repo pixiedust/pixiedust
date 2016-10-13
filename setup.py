72fbc9b267264b9b17a7524208c3424cb01a9a82
Mark Watson <mark.watson.x@gmail.com>
2016-10-13 16:19:30 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pixiedust',
-      version='0.39',
+      version='0.40',
       description='Misc helpers for Spark Python Notebook',
       url='https://github.com/ibm-cds-labs/pixiedust',
       install_requires=['maven-artifact','mpld3'],
