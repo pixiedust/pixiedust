ff79e776ae5aef064e3c36d7fa5f480bccdcce5f
David Taieb <david_taieb@us.ibm.com>
2016-11-23 15:17:01 -0500

    Fixed typo

--- pixiedust/packageManager/package.py
+++ pixiedust/packageManager/package.py
@@ -1,3 +1,19 @@
+# -------------------------------------------------------------------------------
+# Copyright IBM Corp. 2016
+# 
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -------------------------------------------------------------------------------
+
 import os
 
 class Package(object):
@@ -34,7 +50,7 @@ class Package(object):
 
     def getFilePath(self, filename=None):
         if  self.uri:
-            return os.path.join(dir, uri.split("/")[-1])
+            return os.path.join(filename, self.uri.split("/")[-1])
 
         if not filename:
             filename = self._generateFileName()
