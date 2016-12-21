51262c573a943c1b57c0d9a51657a24d28a35a2c
David Taieb <david_taieb@us.ibm.com>
2016-12-21 11:54:56 -0500

    Added global option to scala bridge to build into ~/data/libs

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pixiedust',
-      version='0.69',
+      version='0.70',
       description='Misc helpers for Spark Python Notebook',
       url='https://github.com/ibm-cds-labs/pixiedust',
       install_requires=['mpld3','lxml'],
