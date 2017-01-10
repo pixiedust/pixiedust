170ca0af6e7b35f2ba7de89bf1fd69d17d3c8b0d
David Taieb <david_taieb@us.ibm.com>
2017-01-09 22:37:40 -0500

    Buffer spark events and process them in a separate thread

--- pixiedust/utils/templates/sparkJobProgressMonitor/taskStart.js
+++ pixiedust/utils/templates/sparkJobProgressMonitor/taskStart.js
@@ -1,7 +1,7 @@
 function incProgress(id){
     var n = $(id);
-    n.attr("value", parseInt( n.attr("value")) + 1);
+    n.attr("value", parseInt( n.attr("value")) + {{increment}});
 }
 incProgress("#progress{{prefix}}{{data["stageId"]}}");
 incProgress("#pm_overallProgress{{prefix}}");
-$("#progressNumTask{{prefix}}{{data["stageId"]}}").text("{{data["taskInfo"]["index"]+1}}")
+$("#progressNumTask{{prefix}}{{data["stageId"]}}").text("{{data["taskInfo"]["index"]+1}}");
