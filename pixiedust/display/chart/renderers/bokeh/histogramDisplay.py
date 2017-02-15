28ea26c8b78092b13b786d0b37c82c869f615cb2
Chetna Warade <warade@gmail.com>
2017-02-15 12:59:06 -0500

    Gridplotting bokeh histograms
    Compute # of rows depending on the user selections, # of columns == 2
    as 800 - 900 plot_width is legible, beyond that a scroll bar appears
    hiding histogram.

--- pixiedust/display/chart/renderers/bokeh/histogramDisplay.py
+++ pixiedust/display/chart/renderers/bokeh/histogramDisplay.py
@@ -54,20 +54,24 @@ class HistogramRenderer(BokehBaseDisplay):
         ]
 
     def canRenderChart(self):
-        valueFields = self.getValueFields()
-        if len(valueFields) != 1:
-            return (False, "You can only select one value")
-        else:
-            return (True, None)
+        return (True, None)
 
     def getExtraFields(self):
         color = self.options.get("color")
         return [color] if color is not None else []
     
     def createBokehChart(self):     
-        
         binsize = int(self.options.get('binsize', 10))  
-        return Histogram(self.getWorkingPandasDataFrame(), values=self.getValueFields()[0],
-            plot_width=800,bins=binsize, color=self.options.get("color"), 
-            xgrid=True, ygrid=True
-        )
+        valueFields = self.getValueFields()
+        histograms = []
+        if len(valueFields) != 1:
+            for i in valueFields:
+                histograms.append(Histogram(self.getWorkingPandasDataFrame(), values=i,
+            plot_width=400, plot_height=400,bins=binsize, color=self.options.get("color"), 
+            xgrid=True, ygrid=True))
+
+            return histograms
+        else:
+            return Histogram(self.getWorkingPandasDataFrame(), values=self.getValueFields()[0],
+            plot_width=400,plot_height=400,bins=binsize, color=self.options.get("color"), 
+            xgrid=True, ygrid=True)
