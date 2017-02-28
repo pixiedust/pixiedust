1b77a9d724944cfd1da015b479a8f872268b5b6c
Chetna Warade <warade@gmail.com>
2017-02-28 16:09:40 -0500

    Stash to Cloudant - manual steps
    Inform user to setup cloudant spark before the first use at
    https://ibm-cds-labs.github.io/pixiedust/install.html#stash-to-cloudant
    
    Issue #108 and #171

--- pixiedust/services/stashCloudant.py
+++ pixiedust/services/stashCloudant.py
@@ -34,8 +34,8 @@ class StashCloudantHandler(Display):
 
         config = ShellAccess.sc._conf.getAll()
         if not any("spark.jars" in s for s in config):
-            self._addHTML("Please set PYSPARK_SUBMIT_ARGS to --jars local_dir_path/cloudant-spark.jar in kernel.json")
-            self.debug("Please set PYSPARK_SUBMIT_ARGS to --jars <local_dir_path>/cloudant-spark.jar in kernel.json")
+            self._addHTML("Please setup one time manual steps to configure stashing to Cloudant database at https://ibm-cds-labs.github.io/pixiedust/install.html#stash-to-cloudant")
+            self.debug("Please setup one time manual steps to configure stashing to Cloudant database at https://ibm-cds-labs.github.io/pixiedust/install.html#stash-to-cloudant")
             self.debug("SparkContext conf:")
             self.debug(config)
             return
