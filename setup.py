521a7b32dc8ba5b36c631efa4fbc855f0251e7a0
David Taieb <david_taieb@us.ibm.com>
2017-03-01 11:41:37 -0500

    version bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='pixiedust',
-	  version='0.85',
+	  version='0.86',
 	  description='Productivity library for Spark Python Notebook',
 	  url='https://github.com/ibm-cds-labs/pixiedust',
 	  install_requires=['mpld3','lxml'],
