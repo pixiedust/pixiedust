75bd3f0b2786ef769c3d8bb3ad187ba8d14318d8
Mike Broberg <mikebroberg@gmail.com>
2017-06-09 15:47:13 -0400

    Updated DSX docs to include special meta tags

--- docsrc/source/htmlFragments.py
+++ docsrc/source/htmlFragments.py
@@ -8,7 +8,6 @@ from bs4 import BeautifulSoup
 for fn in os.listdir('.') :
      if os.path.isfile(fn) :
         tmp = os.path.splitext(fn)
-        # ('fn', '.rst')
 
         if tmp[1] == ".rst" :
             source = fn
@@ -19,12 +18,6 @@ for fn in os.listdir('.') :
 
             for line in fh :
                 words = line.split()
-                #if '::' in words :
-                    #nextline = next(fh)
-                    #print(nextline)
-                    #while (nextline and nextline.startswith('  ')) :
-                        #print (nextline, end='', file=writeOut,)
-                        #nextline = next(fh)
                 if ("container::" or "raw::") in words :
                     continue
                 if source == "index.rst" and (("Get" in words and "Started" in words) or "-----------" in words) :
@@ -33,14 +26,13 @@ for fn in os.listdir('.') :
                     break
                 else :
                     print (line, end='', file=writeOut,)
-                    #print (line, end='',)
 
             fh.close()
             writeOut.close()
             subprocess.call(['rst2html5.py', parsed, dest])
             subprocess.call(['rm', parsed])
 
-# Now fix the HTML files, stripping away <style> tag and extra tag attributes
+# Fix the HTML files, stripping away <style> tag and extra tag attributes
 for fn in os.listdir('clean-for-dsx') :
     filePath = 'clean-for-dsx/'+fn
     
@@ -49,15 +41,14 @@ for fn in os.listdir('clean-for-dsx') :
 
         if tmp[1] == ".html" :
             fh = open(filePath)
-            #data=fh.read().replace('\n', '')
             data=fh.read()
             fh.close()
 
             soup = BeautifulSoup(data, "lxml")
-            # Remove style elements
-            to_extract = soup.findAll('style')
-            for element in to_extract :
-                element.extract()
+            # Remove <style> and <meta>
+            to_delete = soup.findAll(['style', 'meta'])
+            for element in to_delete :
+                element.decompose()
 
             # Remove all tag attributes except for src and href; except for <span> edge-case for code blocks
             for tag in soup.findAll(True) :
@@ -76,8 +67,31 @@ for fn in os.listdir('clean-for-dsx') :
                 # Base case: remove all attributes
                 tag.attrs = {}
 
+            # Add DSX metadata to <head>
+            header = soup.head
+            attribs = []
+            attribs.append({"name": "copyright", "content": "&#xA9;Copyright IBM Corporation 2017"})
+            attribs.append({"name": "DC.Rights.Owner", "content": "&#xA9;Copyright IBM Corporation 2017"})
+            attribs.append({"name": "dcterms.rights", "content": "&#xA9; Copyright IBM Corporation 2016, 2017"})
+            # Update with date of last release note entry
+            attribs.append({"name": "DC.date", "content": "2017-06-05"})
+            for entry in attribs :
+                new_tag = soup.new_tag("meta", content=entry["content"])
+                new_tag["name"] = entry["name"]
+                header.append(new_tag)
+
+            # Write the updated soup
             fh = open(filePath, 'w')
             fh.write(soup.prettify().encode("utf-8"))
+            fh.close()
+
+            # Fix the soup's stupidly escaped ampersands within strings
+            fh = open(filePath, 'r')
+            post_soup = fh.read()
+            fh.close()
+            updated_soup = post_soup.replace("&amp;#xA9;", "&#xA9;")
+            fh = open(filePath, 'w')
+            fh.write(updated_soup)
             fh.close()
 
 print ("Yeah, dog!")
