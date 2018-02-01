9bbd640f6fccfaebfa5e82b6b51b8af3178aaf6f
Raj Singh <rrsingh@us.ibm.com>
2018-02-01 14:40:45 -0500

    include no schema info as a param

--- pixiedust/display/chart/renderers/table/TableDisplay.py
+++ pixiedust/display/chart/renderers/table/TableDisplay.py
@@ -8,4 +8,4 @@ from pixiedust.utils import Logger
 class TableDisplay(TableBaseDisplay):
 
     def doRenderChart(self):
-        return self.renderTemplate("table.html", wpdf=self.getWorkingPandasDataFrame())
+        return self.renderTemplate("table.html", wpdf=self.getWorkingPandasDataFrame(), table_noschema=self.options.get("table_noschema", "false"))
